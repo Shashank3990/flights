aa17024643507f540f29658b721c92b0f8f17fbc
Shashank3990 <shashank399swami@gmail.com>
2025-12-11 16:42:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -132,9 +132,3 @@ app.get("/", (_req, res) => res.send("Flight backend running"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`API running on port ${PORT}`));
-
-
-
-
-
-
